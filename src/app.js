f52f1f868c524940e403bf19855b424b01c7e040
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-06-06 21:15:59 +0000

    Adding script for domain-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "a", "one", "for", "other", "each", "all"];
+  let adjective = [
+    "great",
+    "big",
+    "wonderful",
+    "spectacular",
+    "impressive",
+    "stunning"
+  ];
+  let noun = [
+    "idea",
+    "innovate",
+    "spaces",
+    "star",
+    "ventures",
+    "spaces",
+    "vision"
+  ];
+
+  let domainExtension = [".com", ".es", ".fr", ".de", ".org", ".edu", ".co.uk"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domainExtension.length; l++) {
+          console.log(pronoun[i] + adjective[j] + noun[k] + domainExtension[l]);
+        }
+      }
+    }
+  }
 };
