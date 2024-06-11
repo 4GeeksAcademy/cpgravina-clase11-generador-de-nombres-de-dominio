c82b9dc52741b5eb491b4bbcdfdcedf979715e9d
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-06-11 13:26:35 +0000

    adding small changes to the feature

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoun = ["the", "our", "a", "one", "for", "other", "each", "all"];
-  let adjective = [
+  const pronounList = ["the", "our", "a", "one", "for", "other", "each", "all"];
+  const adjectiveList = [
     "great",
     "big",
     "wonderful",
@@ -16,7 +16,7 @@ window.onload = function() {
     "impressive",
     "stunning"
   ];
-  let noun = [
+  const nounList = [
     "idea",
     "innovate",
     "spaces",
@@ -26,13 +26,26 @@ window.onload = function() {
     "vision"
   ];
 
-  let domainExtension = [".com", ".es", ".fr", ".de", ".org", ".edu", ".co.uk"];
+  let domainExtensionList = [
+    ".com",
+    ".es",
+    ".fr",
+    ".de",
+    ".org",
+    ".edu",
+    ".co.uk"
+  ];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adjective.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < domainExtension.length; l++) {
-          console.log(pronoun[i] + adjective[j] + noun[k] + domainExtension[l]);
+  for (let i = 0; i < pronounList.length; i++) {
+    for (let j = 0; j < adjectiveList.length; j++) {
+      for (let k = 0; k < nounList.length; k++) {
+        for (let l = 0; l < domainExtensionList.length; l++) {
+          let domainNameGenerator =
+            pronounList[i] +
+            adjectiveList[j] +
+            nounList[k] +
+            domainExtensionList[l];
+          console.log(domainNameGenerator);
         }
       }
     }
